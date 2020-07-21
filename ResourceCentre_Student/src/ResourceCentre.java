d9e6d104e5b403157d9fa4049231efeb9424617e
19013989 <19013989@DESKTOP-PBSPQ6T>
2020-07-21 13:46:16 +0800

    Added returnChromeBook method

--- ResourceCentre_Student/src/ResourceCentre.java
+++ ResourceCentre_Student/src/ResourceCentre.java
@@ -306,7 +306,19 @@ public class ResourceCentre {
 	public static void returnChromebook(ArrayList<Chromebook> chromebookList) {
 		// write your code here
 		// write your code here
+		ResourceCentre.viewAllChromebook(chromebookList);
+        String tag = Helper.readString("Enter asset tag > ");
+        Boolean isReturned = doReturnChromebook(chromebookList, tag);
+       
+        if (isReturned == false) {
+            System.out.println("Invalid asset tag");
+        } else {
+            System.out.println("Chromebook " + tag + " returned");
+        }
+    }
+		
+		
 	}
 
 
-}
+
